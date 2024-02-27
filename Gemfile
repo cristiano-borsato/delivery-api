e3f22fda115c47121ecd28d39c2fb8f7b816b5c7
Cristiano Borsato <cris.borsato@gmail.com>
2024-02-27 19:12:59 -0300

    Criação de testes

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "rspec-rails"
@@ -61,3 +63,6 @@ group :development do
   # gem "spring"
 end
 
+gem "shoulda-matchers"
+
+gem "faker"
---
+++ spec/models/store_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+# RSpec.describe Store, type: :model do
+#   describe "validations" do
+#     it "should be valid when name is filled" do
+#       store = Store.new name: "Greatest store ever!"
+#     # expect(store.valid?).to eq true
+#       expect(store).to be_valid
+#     end
+#   end
+# end
+
+# RSpec.describe Store, type: :model do
+#   describe "validations" do
+#     it "should validate presence of name" do
+#       store = Store.new    
+#       expect(store).to_not be_valid
+#     end
+#   end
+# end
+
+# RSpec.describe Store, type: :model do
+#   describe "validations" do
+#     it do         
+#       should validate_presence_of :name
+#     end
+#   end
+# end
+
+RSpec.describe Store, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_length_of(:name).is_at_least(3) }   
+  end
+end
