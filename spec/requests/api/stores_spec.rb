f1cfc88a286ce758ac0b5fddbbd126c1945f1e12
Cristiano Borsato <cris.borsato@gmail.com>
2024-03-21 19:16:19 -0300

    Cria permissionamento para admin e registrations_controller

--- spec/requests/api/stores_spec.rb
+++ spec/requests/api/stores_spec.rb
@@ -1,13 +1,33 @@
 require "rails_helper"
 
+
 RSpec.describe "/stores", type: :request do
+
+    let(:user){
+    user = User.new(
+      email: "user@example.com", password: "123456", password_confirmation: "123456"
+    )
+    user.save!
+    user
+    }
+  
+    let(:valid_attributes) {
+        {name: "New Store", user: user}
+    }
+
+    before {
+        sign_in(user)
+    }
+    
     describe "GET /show" do
         it "renders a successful response with stores data" do
-            store = Store.create! name: "New Store"
+            store = Store.create! valid_attributes
+            sign_in user
             get "/stores/#{store.id}", headers: {"Accept" => "application/json"}
             json = JSON.parse(response.body)
-
+            
             expect(json["name"]).to eq "New Store"
         end
     end
+    
 end
