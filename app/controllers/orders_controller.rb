3b9c064705dac68b4b72b6f966c1cd29f7932a98
Cristiano Borsato <cris.borsato@gmail.com>
2024-06-28 16:05:32 -0300

    Adicionar store_id ao view de orders(index.json.jbuilder)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,13 +2,14 @@ class OrdersController < ApplicationController
   skip_forgery_protection
   before_action :authenticate!, :only_buyers!
 
+
   def create
   
     # >> Este código é equivalente a linha logo abaixo
     # @order = Order.new(order_params)
     # @order.buyer = current_user
     
-    @order = Order.new(order_params) { |o| o.buyer = current_user }
+    @order = Order.new(order_params) { |o| o.buyer = current_user }    
 
     if @order.save
       render :create, status: :created
